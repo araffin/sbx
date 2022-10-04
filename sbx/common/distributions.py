546e642a2a6f6ddf869c32a161724d1004f8a729
Antonin Raffin <antonin.raffin@dlr.de>
2022-10-04 16:37:08 +0200

    Revert "WIP gSDE"
    This reverts commit c7de49157ec264e466300d4a05436e46837208fa.

--- sbx/common/distributions.py
+++ sbx/common/distributions.py
@@ -24,17 +24,3 @@ class TanhTransformedDistribution(tfd.TransformedDistribution):
         td_properties = super()._parameter_properties(dtype, num_classes=num_classes)
         del td_properties["bijector"]
         return td_properties
-
-
-class StateDependentNoiseDistribution(tfd.MultivariateNormalDiag):
-    def __init__(self, loc=None, scale_diag=None, std=None, latent_sde=None):
-        super().__init__(loc=loc, scale_diag=scale_diag)
-        self.std = std
-        self.latent_sde = latent_sde
-
-    def sample(self, seed=None) -> jnp.ndarray:
-        exploration_mat = tfd.MultivariateNormalDiag(
-            loc=jnp.zeros_like(self.std),
-            scale_diag=self.std,
-        ).sample(seed=seed)
-        return self.latent_sde @ exploration_mat
