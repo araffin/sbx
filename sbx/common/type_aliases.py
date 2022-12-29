2d8f50ad6d6f929044fb3cd9ebadbfee8fc3d8dc
Antonin Raffin <antonin.raffin@ensta.org>
2022-12-29 11:02:55 +0100

    Save WIP

--- sbx/common/type_aliases.py
+++ sbx/common/type_aliases.py
@@ -15,3 +15,12 @@ class ReplayBufferSamplesNp(NamedTuple):
     next_observations: np.ndarray
     dones: np.ndarray
     rewards: np.ndarray
+
+
+class RolloutBufferSamplesNp(NamedTuple):
+    observations: np.ndarray
+    actions: np.ndarray
+    old_values: np.ndarray
+    old_log_prob: np.ndarray
+    advantages: np.ndarray
+    returns: np.ndarray
