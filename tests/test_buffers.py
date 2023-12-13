ba597ca550e1a0ebc1e1ad3be041fac749c2fb0d
Antonin RAFFIN <antonin.raffin@ensta.org>
2023-12-13 11:05:43 +0100

    Fix replay buffer device at load time (#20)
    * Fix replay buffer device at load time
    
    * Fix imports
    
    * Update version
    
    * Reformat and add test
    
    * Fix test
    
    * Fix for mypy

---
+++ tests/test_buffers.py
@@ -0,0 +1,14 @@
+import pytest
+import torch as th
+
+from sbx import SAC
+
+
+def test_force_cpu_device(tmp_path):
+    if not th.cuda.is_available():
+        pytest.skip("No CUDA device")
+    model = SAC("MlpPolicy", "Pendulum-v1", buffer_size=200)
+    assert model.replay_buffer.device == th.device("cpu")
+    model.save_replay_buffer(tmp_path / "replay")
+    model.load_replay_buffer(tmp_path / "replay")
+    assert model.replay_buffer.device == th.device("cpu")
