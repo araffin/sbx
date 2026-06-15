6cd38e66b237751a928a94e34b6f45ab92edaf9b
Antonin RAFFIN <antonin.raffin@ensta.org>
2026-06-15 11:36:27 +0200

    Allow jax v0.10.x

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     package_data={"sbx": ["py.typed", "version.txt"]},
     install_requires=[
         "stable_baselines3>=2.9.0a3,<3.0",
-        "jax>=0.4.24,<0.10.0",
+        "jax>=0.4.24,<0.11.0",
         "jaxlib",
         "flax",
         "optax",
